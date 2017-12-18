7d79ee186b40ad2e8370cdc125f53e05249b998d
dinesh <dinesh@Dinesh>
2017-12-18 16:40:00 +0530

    First Commit

---
+++ RaceDynamics/src/main/java/com/spring/dao/UserDAOImpl.java
@@ -0,0 +1,47 @@
+package com.spring.dao;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.model.Users;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+	
+		public UserDAOImpl()
+		{}
+	
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	
+
+	public UserDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory=sessionFactory;
+	}
+
+
+	public boolean saveUser(Users user) {
+		
+		
+		try {
+			Session	session =sessionFactory.getCurrentSession();
+			Transaction tx=session.beginTransaction();
+			session.saveOrUpdate(user);
+			tx.commit();
+			return true;
+		} catch (HibernateException e) {
+			
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+
+}
---
+++ RaceDynamics/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<body>
+<div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <input type="checkbox" checked="checked"> Remember me
+  </div>
+</body>
+</html>
